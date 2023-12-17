98a273a493de191fdc5a5ff91d3dac330c037b84
Ernest Chiu <ernest@withpersona.com>
2023-12-17 11:32:30 -0500

    added linter and formatter for ruby and typescript

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -3,8 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.api.nvim_set_option("clipboard","unnamed")
+vim.api.nvim_set_option("clipboard", "unnamed")
 local keymap = vim.api.nvim_set_keymap
-keymap('i', 'jk', '<Esc>', {})
-
-
+keymap("i", "jk", "<Esc>", {})
