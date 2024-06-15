4fd677a0560c0975c3cbbea4af3dc59ee5e4e755
Ernest Chiu <ernestchiu14@gmail.com>
2024-06-15 13:28:47 -0700

    added other.lua to lazyVim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,19 +1,32 @@
 return {
   "stevearc/oil.nvim",
+  opts = {},
+  event = "VeryLazy",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    {
-      "-",
-      function()
-        require("oil").open()
-      end,
-      desc = "[F]ormat buffer",
-    },
-  },
-  opts = {
-    default_file_explorer = true,
-    view_options = {
-      show_hidden = true,
-    },
-  },
+  config = function()
+    require("oil").setup({
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-h>"] = false,
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = false,
+        ["<C-q>"] = "actions.send_to_qflist",
+        ["<C-a>"] = "actions.add_to_qflist",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+    })
+
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
 }
